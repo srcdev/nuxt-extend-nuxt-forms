87c70681005172b6a668d4fe74e03e652dabde14
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2024-11-14 22:38:36 +0000

    Package update to fcorrect checkbox and radio selected issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/icon"],
-  extends: ["srcdev-nuxt-forms", "srcdev-nuxt-components"],
+  css: ['modern-normalize', './assets/styles/index.css'],
+  modules: ['@nuxt/icon'],
+  extends: ['srcdev-nuxt-forms', 'srcdev-nuxt-components'],
 });
