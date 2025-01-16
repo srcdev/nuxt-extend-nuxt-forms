3e9cf356e26e238e0a9743ae4144e4a0beb72e6d
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2025-01-16 21:05:42 +0000

    Updated to https

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  devServer: {
+    https: true,
+  },
   app: {
     head: {
       htmlAttrs: {
