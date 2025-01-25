03511e3ac5a220445ef64809242c0f5d565e10ce
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2025-01-25 22:43:11 +0000

    Example prompts added

--- pages/components/ui/display-prompt.vue
+++ pages/components/ui/display-prompt.vue
@@ -4,15 +4,37 @@
       <template #content>
         <LayoutRow tag="div" variant="popout" :styleClassPassthrough="['mbe-20']">
           <h2 class="heading-2">Display Prompts</h2>
+          <DisplayPromptCore theme="success" :dismissible="true" :style-class-passthrough="['mbs-20']">
+            <template #icon>
+              <Icon name="akar-icons:check" class="icon" />
+            </template>
+            <template #title>Success Prompt Title with content (Dismissable)</template>
+            <template #content>This is prompt content, it can contain html or plain text.</template>
+          </DisplayPromptCore>
 
-          <DisplayPromptCore theme="error" :dismissible="true" :style-class-passthrough="['mbe-20', 'mbs-20']">
+          <DisplayPromptCore theme="info" :dismissible="true" :style-class-passthrough="['mbs-20']">
+            <template #icon>
+              <Icon name="akar-icons:info" class="icon" />
+            </template>
+            <template #title>Info Prompt Title with content (Dismissable)</template>
+            <template #content>This is prompt content, it can contain html or plain text.</template>
+          </DisplayPromptCore>
+
+          <DisplayPromptCore theme="error" :dismissible="true" :style-class-passthrough="['mbs-20']">
             <template #icon>
               <Icon name="akar-icons:circle-alert" class="icon" />
             </template>
             <template #title>Error Prompt Title with content (Dismissable)</template>
             <template #content>This is prompt content, it can contain html or plain text.</template>
           </DisplayPromptCore>
-          <p class="body-normal">This text sit beneath the prompt.</p>
+
+          <DisplayPromptCore theme="warning" :dismissible="true" :style-class-passthrough="['mbs-20']">
+            <template #icon>
+              <Icon name="akar-icons:circle-alert" class="icon" />
+            </template>
+            <template #title>Warning Prompt Title with content (Dismissable)</template>
+            <template #content>This is prompt content, it can contain html or plain text.</template>
+          </DisplayPromptCore>
         </LayoutRow>
       </template>
     </NuxtLayout>
